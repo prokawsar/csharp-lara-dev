92f1d5a88ea4621ffb8813e91e1baf75c4e43468
Kawsar Ahmed <mkawsar10@gmail.com>
2018-10-13 00:12:41 +0600

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -64,7 +64,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <a target="_blank" href="https://github.com/prokawsar" className="github-corner" aria-label="View source on Github">
+        <a target="_blank" href="https://github.com/prokawsar/csharp-lara-dev" className="github-corner" aria-label="View source on Github">
           <svg width="80" height="80" viewBox="0 0 250 250" aria-hidden="true">
             <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
             <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
@@ -74,8 +74,8 @@ class App extends Component {
           </svg>
         </a>
         <br />
-        <div className="hd1">Laravel and ASP.net developer from Dhaka, Bangladesh </div>
-        <div className="hd-small">Well experienced laravel and asp.net developer from Bangladesh.</div>
+        <div className="hd1">Laravel and JavaScript Developer</div>
+        <div className="hd-small">Well experienced laravel and javascript developer from Worldwide.</div>
 
         <div className="search-div">
           <input
